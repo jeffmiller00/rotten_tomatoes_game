778c56bb03c1d34f56a22783785a6a3214637d39
Prelang Builder <builder@prelang.com>
2014-04-12 19:46:15 +0000

    Initial commit; clean Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RottenTomatoesGame::Application.config.secret_key_base = '65a8e2974648fc2315e88743d6d10ee966be3cd47301bafd58c29f3864c82e031ace126e988e682a1f9b997030f46bf41d282c90e35b6b5321f2b16b3e596a73'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RottenTomatoesGame::Application.config.session_store :cookie_store, key: '_rotten_tomatoes_game_session'
